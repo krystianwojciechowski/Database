c2907d66ef499ee07534d2175eee70fbe392c7b3
Krystian Wojciechowski <krystian.wojciechowski31@gmail.com>
2022-04-27 19:46:44 +0200

    base class for mongo DTOs

---
+++ Database/BaseDTO/MongoBaseDTO.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.BaseDTO
+{
+    internal record MongoBaseDTO
+    {
+        public ObjectId Id { get; init; }
+    }
+}
--- Database/BaseEntities/BaseMongoEntity.cs
+++ Database/BaseEntities/BaseMongoEntity.cs
@@ -13,5 +13,19 @@ namespace Database.BaseEntities
         [BsonElement("_id")]
         [BsonId]
         public ObjectId Id { get; set; }
+
+
+        //Two entities are the same when they share id. When field was edited, entity remains the same entity just with differen value for field
+        public override bool Equals(object? obj)
+        {
+            var item = obj as BaseMongoEntity;
+
+            if (item == null)
+            {
+                return false;
+            }
+
+            return item.Id.Equals(Id);
+        }
     }
 }
